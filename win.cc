4f3266d97ff70372367a2db89c781433a3fa2afe
DevHyperCoder <arduinoleo88@gmail.com>
2021-08-21 20:40:32 +0530

    Add inc and dec with limits

--- win.cc
+++ win.cc
@@ -1,9 +1,65 @@
 #include "win.h"
+#include "gtkmm/enums.h"
+#include "sigc++/functors/mem_fun.h"
+#include <string>
 
 /*
  * Basic constructor to init label value
  */
 Win::Win() {
-    hello_label.set_label("Hello!");
-    set_child(hello_label);
+
+    inc_btn.signal_clicked().connect(
+        sigc::mem_fun(*this,&Win::inc_cnt)
+    );
+
+    inc_label.set_label("INCREMENT");
+    inc_btn.set_child(inc_label);
+
+    dec_btn.signal_clicked().connect(
+        sigc::mem_fun(*this,&Win::dec_cnt)
+    );
+
+    dec_label.set_label("DECREMENT");
+    dec_btn.set_child(dec_label);
+
+    btn_container.set_orientation(Gtk::Orientation::HORIZONTAL);
+    btn_container.set_homogeneous();
+    btn_container.append(inc_btn);
+    btn_container.append(dec_btn);
+
+    main_container.set_orientation(Gtk::Orientation::VERTICAL);
+    main_container.append(btn_container);
+    main_container.append(count_label);
+
+    set_child(main_container);
+
+    update_ui();
+}
+
+void Win::inc_cnt() {
+    count +=1;
+    update_ui();
+}
+
+void Win::dec_cnt() {
+    count -=1;
+    update_ui();
+}
+
+void Win::update_ui() {
+    // int -> str and update label
+    std::string str_cnt = std::to_string(count);
+    count_label.set_label(str_cnt);
+
+    // Set both buttons to enabled state
+    inc_btn.set_sensitive();
+    dec_btn.set_sensitive();
+
+    if (count == max_cnt) {
+        inc_btn.set_sensitive(false);
+    }
+
+    if(count == min_cnt) {
+        dec_btn.set_sensitive(false);
+    }
 }
--- win.h
+++ win.h
@@ -1,8 +1,12 @@
 #ifndef WIN_H
 #define WIN_H
 
+#include "gtkmm/button.h"
 #include "gtkmm/label.h"
 #include <gtkmm.h>
+
+#define MAX_CNT 10
+#define MIN_CNT 0
 
 #endif // WIN_H
 
@@ -13,6 +17,22 @@ class Win : public Gtk::Window {
 public:
     Win();
 protected:
-    Gtk::Label hello_label;
+    int max_cnt = MAX_CNT;
+    int min_cnt = MIN_CNT;
 
+    Gtk::Box main_container;
+    Gtk::Box btn_container;
+
+    Gtk::Label count_label;
+    Gtk::Label inc_label;
+    Gtk::Label dec_label;
+
+    Gtk::Button inc_btn;
+    Gtk::Button dec_btn;
+
+    int count = 0;
+
+    void update_ui();
+    void inc_cnt();
+    void dec_cnt();
 };

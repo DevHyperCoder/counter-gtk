4ba22a8af3747e8be01804b00ea9409fd9731f93
DevHyperCoder <arduinoleo88@gmail.com>
2021-08-21 21:07:33 +0530

    Add user input for min_cnt and max_cnt

--- win.cc
+++ win.cc
@@ -1,6 +1,8 @@
 #include "win.h"
 #include "gtkmm/enums.h"
 #include "sigc++/functors/mem_fun.h"
+#include <iostream>
+#include <stdexcept>
 #include <string>
 
 /*
@@ -27,8 +29,20 @@ Win::Win() {
     btn_container.append(inc_btn);
     btn_container.append(dec_btn);
 
+    min_cnt_entry.set_placeholder_text("0 (min)");
+    max_cnt_entry.set_placeholder_text("10 (max)");
+
+    min_cnt_entry.signal_changed().connect(sigc::mem_fun(*this,&Win::update_min_cnt));
+    max_cnt_entry.signal_changed().connect(sigc::mem_fun(*this,&Win::update_max_cnt));
+
+    entry_container.set_homogeneous();
+    entry_container.append(min_cnt_entry);
+    entry_container.append(max_cnt_entry);
+
     main_container.set_orientation(Gtk::Orientation::VERTICAL);
     main_container.append(btn_container);
+    main_container.append(entry_container);
+    main_container.append(entry_status_label);
     main_container.append(count_label);
 
     set_child(main_container);
@@ -51,6 +65,15 @@ void Win::update_ui() {
     std::string str_cnt = std::to_string(count);
     count_label.set_label(str_cnt);
 
+    entry_status_label.set_label(entry_status);
+
+    if(entry_status != "") {
+        inc_btn.set_sensitive(false);
+        dec_btn.set_sensitive(false);
+
+        return;
+    }
+
     // Set both buttons to enabled state
     inc_btn.set_sensitive();
     dec_btn.set_sensitive();
@@ -63,3 +86,34 @@ void Win::update_ui() {
         dec_btn.set_sensitive(false);
     }
 }
+
+void Win::update_max_cnt() {
+    auto max_str = max_cnt_entry.get_text();
+
+    try {
+        max_cnt = std::stoi(max_str);
+        entry_status = "";
+    } catch(std::invalid_argument const &e) {
+        entry_status = "Invalid max count!";
+    } catch(std::out_of_range const &e ) {
+        entry_status = "Too long for me!";
+    }
+
+    update_ui();
+}
+
+void Win::update_min_cnt() {
+    auto min_str = min_cnt_entry.get_text();
+
+    try {
+        min_cnt = std::stoi(min_str);
+        entry_status = "";
+    } catch(std::invalid_argument const &e) {
+        entry_status = "Invalid min count!";
+    } catch(std::out_of_range const &e ) {
+        entry_status = "Too long for me!";
+    }
+
+    update_ui();
+}
+
--- win.h
+++ win.h
@@ -2,6 +2,7 @@
 #define WIN_H
 
 #include "gtkmm/button.h"
+#include "gtkmm/entry.h"
 #include "gtkmm/label.h"
 #include <gtkmm.h>
 
@@ -22,17 +23,27 @@ protected:
 
     Gtk::Box main_container;
     Gtk::Box btn_container;
+    Gtk::Box entry_container;
 
     Gtk::Label count_label;
     Gtk::Label inc_label;
     Gtk::Label dec_label;
+    Gtk::Label entry_status_label;
 
     Gtk::Button inc_btn;
     Gtk::Button dec_btn;
 
+    Gtk::Entry min_cnt_entry;
+    Gtk::Entry max_cnt_entry;
+
     int count = 0;
+    std::string entry_status = "";
 
     void update_ui();
+
     void inc_cnt();
     void dec_cnt();
+
+    void update_min_cnt();
+    void update_max_cnt();
 };
